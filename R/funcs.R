c1778a1afe050e7e8a5b5e0d90cd0980bce18e24
fawda123 <mbafs2012@gmail.com>
2024-07-25 11:18:03 -0400

    add trophy for A, A- scores #1

--- R/funcs.R
+++ R/funcs.R
@@ -290,7 +290,14 @@ evaltrntab_fun <- function(evalgrp){
       columns = sstrunum,
       after = `Short Shoot Density reported (average)`
     ) |> 
-    gt::cols_align('left')
+    gt::cols_align('left')# |> 
+    # gt::opt_interactive(
+    #   use_pagination = F, 
+    #   use_pagination_info = F,
+    #   use_sorting = F, 
+    #   use_filters = T,
+    #   pagination_type = 'simple'
+    # )
   
   return(out)
   
@@ -468,6 +475,8 @@ card_fun <- function(evalgrp, grp, allgrpscr, vr = c('Abundance', 'Blade Length'
     ) |> 
     plotly::config(displayModeBar = F)
 
+  troph <- troph_fun(scr)
+  
   txtdev <- dplyr::case_when(
     scr == 'A' ~ 'reported values deviate very little from the average',
     hiscr ~ 'reported values deviate a little from the average',
@@ -485,7 +494,7 @@ card_fun <- function(evalgrp, grp, allgrpscr, vr = c('Abundance', 'Blade Length'
     sgndff == '-' & !hiscr & scr != 'A' ~ ', much lower', 
     T ~ ''
   )
-  txtdsc <- paste0('<span><h3><b>', scr, '</b></h3><h4> ', txtdev, txtdir, '</h4></span>')
+  txtdsc <- paste0('<span><h3><b>', scr, '&nbsp;', troph, '</b></h3><h4> ', txtdev, txtdir, '</h4></span>')
   
   bslib::value_box(
     title = gt::html(paste0('<b>', vr, ' summary</b>')),
@@ -575,8 +584,10 @@ scrsum_fun <- function(allgrpscr, grp){
   spcs2 <- rep('&nbsp;', 20) |> 
     paste0(collapse = '')
   
+  troph <- troph_fun(totscr)
+  
   # convert all to html
-  tottxt <- paste0('<h1>', spcs1, '<b>', as.character(totscr), '</b>', ' overall score', '</h1>')
+  tottxt <- paste0('<h1>', spcs1, '<b>', as.character(totscr), '</b>', ' overall score', troph, '</h1>')
   higher <- paste0('<h3>', spcs2, '<b>', higher, '</b>', ' groups had a higher score', '</h3>')
   higher <- ifelse(grepl('One', higher), gsub('groups', 'group', higher), higher)
   lower <- paste0('<h3>', spcs2, '<b>', lower, '</b>', ' groups had a lower score', '</h3>')
@@ -625,14 +636,30 @@ scrimp_fun <- function(allgrpscr, grp, vr = c('Abundance', 'Blade Length', 'Shor
     dplyr::filter(grpact == !!grp) |> 
     dplyr::pull(!!vr)
   
+  troph <- troph_fun(scr)
+  
   out <- dplyr::case_when(
     scr %in% c('A', 'A-') ~ "Looks good, keep doing what you're doing!", 
     T ~ paste0('Room for improvement! Learn how to brush up on ', tolower(vr), ' by viewing the link <a target="_blank" href="', vdlnk[[vr]], '">here</a>.') 
   )
-  out <- paste0('<span><h3><b>', scr, '</b></h3><h4>', out, '</h4></span>')
+  out <- paste0('<span><h3><b>', scr, '&nbsp;', troph, '</b></h3><h4>', out, '</h4></span>')
   out <- gt::html(out)
   
   return(out)
   
 }
+ 
+#' Return fontawsome trophy text if scr is an A
+#' 
+#' @param scr character, score
+troph_fun <- function(scr){
   
+  # fontawsome::fa('trophy', fill = 'gold')
+  out <- NULL
+  
+  if(scr %in% c('A', 'A-'))
+    out <- '<svg aria-hidden="true" role="img" viewBox="0 0 576 512" style="height:1em;width:1.12em;vertical-align:-0.125em;margin-left:auto;margin-right:auto;font-size:inherit;fill:gold;overflow:visible;position:relative;"><path d="M400 0H176c-26.5 0-48.1 21.8-47.1 48.2c.2 5.3 .4 10.6 .7 15.8H24C10.7 64 0 74.7 0 88c0 92.6 33.5 157 78.5 200.7c44.3 43.1 98.3 64.8 138.1 75.8c23.4 6.5 39.4 26 39.4 45.6c0 20.9-17 37.9-37.9 37.9H192c-17.7 0-32 14.3-32 32s14.3 32 32 32H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H357.9C337 448 320 431 320 410.1c0-19.6 15.9-39.2 39.4-45.6c39.9-11 93.9-32.7 138.2-75.8C542.5 245 576 180.6 576 88c0-13.3-10.7-24-24-24H446.4c.3-5.2 .5-10.4 .7-15.8C448.1 21.8 426.5 0 400 0zM48.9 112h84.4c9.1 90.1 29.2 150.3 51.9 190.6c-24.9-11-50.8-26.5-73.2-48.3c-32-31.1-58-76-63-142.3zM464.1 254.3c-22.4 21.8-48.3 37.3-73.2 48.3c22.7-40.3 42.8-100.5 51.9-190.6h84.4c-5.1 66.3-31.1 111.2-63 142.3z"/></svg>'
+
+  return(out)
+
+}
